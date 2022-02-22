b3624ef31b28bbd13ad4a7324f2bf24740369730
Samson <95264901+54MMYYY@users.noreply.github.com>
2022-02-22 04:47:17 +0000

    ..

--- p4final.c
+++ p4final.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
-int input(){
+int input_array_size(){
   int a;
-  printf("Enter a number\n");
+  printf("Enter the size of the array: ");
   scanf("%d",&a);
   return a;
 }
-void input_array(int n, int a[n]){
+void input_array(int n, int *a){
+  printf("Enter the values\n");
   for(int i=0;i<n;i++){
-    printf("Enter the element no %d of the array\n",i);
     scanf("%d",&a[i]);
   }
 }
-int array_sum(int n, int a[n]){
+int sum_n_arrays(int n, int a[n]){
   int sum=0;
   for(int i=0;i<n;i++){
     sum += a[i];
   }
   return sum;
 }
-int output(int n,int a[n],int sum){
+void output(int n,int a[n],int sum){
   int i;
   printf("The sum of");
   for(i=0;i<n-1;i++){
     printf("%d + ", a[n]);
   }
-  printf("%d = %d\n", a[i], sum)
+  printf("%d = %d\n", a[i], sum);
 }
 int main(){
-  int n, sum;
-  n=input();
+  int n,sum;
+  n=input_array_size();
+  int a[n];
   input_array(n,a);
-  sum=input_array(n,a);
+  sum = input_array(n,a);
   output(n,a,sum);
-  return 0;
 }
